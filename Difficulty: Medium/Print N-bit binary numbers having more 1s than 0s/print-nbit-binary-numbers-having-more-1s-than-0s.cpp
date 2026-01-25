a26e891e72aec628e23b04324e21766498528a00
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-01-25 13:58:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print N-bit binary numbers having more 1s than 0s/print-nbit-binary-numbers-having-more-1s-than-0s.cpp
@@ -0,0 +1,27 @@
+// User function template for C++
+class Solution {
+  public:
+  void find(int n,vector<string>&ans,string temp,int zero,int one){
+      if(temp.size()==n){
+          ans.push_back(temp);
+          return;
+      }
+ 
+          temp.push_back('1');
+          find(n,ans,temp,zero,one+1);
+          temp.pop_back();
+      
+      if(zero<one){
+          temp.push_back('0');
+          find(n,ans,temp,zero+1,one);
+          temp.pop_back();
+      }
+  }
+    vector<string> NBitBinary(int n) {
+        // Your code goes here
+        vector<string>ans;
+        string temp;
+        find(n,ans,temp,0,0);
+        return ans;
+    }
+};
