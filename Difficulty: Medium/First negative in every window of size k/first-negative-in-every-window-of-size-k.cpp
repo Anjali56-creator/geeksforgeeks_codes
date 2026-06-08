2e6ac3c424c29c17c84c02738792257d12e48191
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-06-08 08:26:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -2,26 +2,28 @@ class Solution {
   public:
     vector<int> firstNegInt(vector<int>& arr, int k) {
         // write code here
-          int n = arr.size();
-        queue<int> q;
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < 0)
-                q.push(i);
-
-            if (i >= k - 1) {
-                while (!q.empty() && q.front() < i - k + 1)
-                    q.pop();
-
-                if (q.empty())
-                    ans.push_back(0);
+        queue<int>q;
+        int n=arr.size();
+        vector<int>ans;
+        for(int i =0 ; i < k ; i++){
+            if(arr[i]<0)
+            q.push(i);
+        }
+        ans.push_back(q.empty() ? 0 : arr[q.front()]);
+        for(int i=k;i<n;i++){
+            if(arr[i]<0)
+            q.push(i);
+            if(q.empty())
+            ans.push_back(0);
+            else {
+                if(q.front()<=i-k)
+                q.pop();
+                if(q.empty())
+                ans.push_back(0);
                 else
-                    ans.push_back(arr[q.front()]);
+                ans.push_back(arr[q.front()]);
             }
         }
-
         return ans;
     }
-   
 };
