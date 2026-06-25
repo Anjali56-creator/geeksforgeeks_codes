1bd31abb9beadbb372b670505c7333769070bbc2
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-06-25 12:44:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int kthSmallest(vector<int> &arr, int k) {
         // code here
-        sort(arr.begin(), arr.end());
-    return arr[k - 1];
+        sort(arr.begin(),arr.end());
+        return arr[k-1];
     }
 };
