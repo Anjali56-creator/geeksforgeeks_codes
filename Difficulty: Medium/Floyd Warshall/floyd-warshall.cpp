f43dd3c38c579f60f64186914d0d7f9a6be06e89
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-07-07 10:56:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n=dist.size();
+        // for(int i=0;i<n;i++)
+        // for(int j=0;j<n;j++)
+        // if(dist[i][j]==-1)
+        // dist[i][j]=1e8;
+        
+        //floyyd warshall
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(dist[i][k]==1e8 || dist[k][j]==1e8)
+                    continue;
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        // for(int i=0;i<n;i++)
+        // for(int j=0;j<n;j++)
+        // if(dist[i][j]==1e8)
+        // dist[i][j]=-1;
+        
+    }
+};
